a93ce8ffa7369469b030711d679628a217da728c
Osada <osadanirmal9@gmail.com>
2026-07-23 23:40:49 +0530

    Configure Security Web Filter Chain for Product Service

---
+++ product-service/src/main/java/com/microservice/product_service/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.microservice.product_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
+        return httpSecurity.build();
+    }
+}
