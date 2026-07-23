92e6d5691996f4e0918e04719bea384c06e9b407
Osada <osadanirmal9@gmail.com>
2026-07-23 23:45:07 +0530

    Configure RestTemplate to propagate JWT token to inventory service

--- order-service/src/main/java/com/microservice/order_service/config/RestTemplateConfig.java
+++ order-service/src/main/java/com/microservice/order_service/config/RestTemplateConfig.java
@@ -3,7 +3,16 @@ package com.microservice.order_service.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Configuration
 public class RestTemplateConfig {
@@ -11,6 +20,16 @@ public class RestTemplateConfig {
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(new ClientHttpRequestInterceptor() {
+            @Override
+            public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+                if (SecurityContextHolder.getContext().getAuthentication() instanceof JwtAuthenticationToken jwtAuthenticationToken) {
+                    request.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer " + jwtAuthenticationToken.getToken().getTokenValue());
+                }
+                return execution.execute(request, body);
+            }
+        });
+        return restTemplate;
     }
 }
