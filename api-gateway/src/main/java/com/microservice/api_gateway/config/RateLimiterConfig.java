d597f7732e24eb255a3b2a15ff6e913c52d64fa8
Osada <osadanirmal9@gmail.com>
2026-07-24 01:16:36 +0530

    Create Rate Limiter KeyResolver using client IP address

---
+++ api-gateway/src/main/java/com/microservice/api_gateway/config/RateLimiterConfig.java
@@ -0,0 +1,17 @@
+package com.microservice.api_gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimiterConfig {
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.justOrEmpty(exchange.getRequest().getRemoteAddress())
+                .map(address -> address.getAddress().getHostAddress())
+                .defaultIfEmpty("anonymous");
+    }
+}
