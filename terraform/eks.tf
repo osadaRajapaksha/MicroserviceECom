2862de6ede4b2e54e5dc2fa81ac847ef79cc7224
Osada <osadanirmal9@gmail.com>
2026-07-29 01:05:50 +0530

    chore(aws): upgrade EKS cluster version to 1.31

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.31"
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
