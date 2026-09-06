be966bf6a09ab91c694d8bb6264df0b066e0c07d
Osada <osadanirmal9@gmail.com>
2026-09-07 04:21:25 +0530

    Initialize recommendation-service module

---
+++ recommendation-service/src/test/java/com/microservice/recommendation_service/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
