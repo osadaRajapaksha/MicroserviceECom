8ffa3ff9734ae64f15502f1de905c93b8398c124
Osada <osadanirmal9@gmail.com>
2026-09-15 10:56:31 +0530

    chore: migrate terraform outputs from aws to azure

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,24 @@
 output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane."
-  value       = module.eks.cluster_endpoint
+  description = "Endpoint for AKS control plane."
+  value       = azurerm_kubernetes_cluster.aks.kube_config.0.host
 }
 
 output "cluster_certificate_authority_data" {
   description = "Base64 encoded certificate data required to communicate with the cluster."
-  value       = module.eks.cluster_certificate_authority_data
+  value       = azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate
 }
 
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
-  value       = module.eks.cluster_name
+  value       = azurerm_kubernetes_cluster.aks.name
 }
 
-output "ecr_repository_urls" {
-  description = "URLs for the created ECR repositories"
-  value       = { for repo in aws_ecr_repository.microservices : repo.name => repo.repository_url }
+output "acr_login_server" {
+  description = "Login server for the Azure Container Registry"
+  value       = azurerm_container_registry.acr.login_server
+}
+
+output "cdn_endpoint" {
+  description = "Endpoint for the Azure CDN"
+  value       = azurerm_cdn_endpoint.frontend.fqdn
 }
