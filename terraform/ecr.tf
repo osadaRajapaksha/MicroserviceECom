949ae1220f20c490efac0c5a709f0cc6b5d7fb54
Osada <osadanirmal9@gmail.com>
2026-09-15 10:55:48 +0530

    feat: replace AWS ECR with Azure ACR

--- terraform/ecr.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "microservices" {
-  for_each             = toset(var.ecr_repositories)
-  name                 = each.value
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
