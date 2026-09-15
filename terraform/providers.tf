45b90d1039aa3de6a6f68d6e17b1bcae3747ba50
Osada <osadanirmal9@gmail.com>
2026-09-15 10:54:04 +0530

    chore: migrate terraform providers from aws to azure

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -13,6 +13,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.aws_region
+provider "azurerm" {
+  features {}
 }
